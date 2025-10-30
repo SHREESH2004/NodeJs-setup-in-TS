1d7a382c97a23b83142a2862025b06192632c971
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-30 18:29:02 +0530

    add concurrently for parallel script execution and update API response structure

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ app.get('/',(req,res)=>{
     res.send('Hello World from TypeScript with Node.js and Express!');
 })
 
+
+app.get('/api/data', (req, res) => {
+    let data = req.body;
+    res.json({ message: 'This is some sample data from the API.', data });
+});
+
 app.listen(PORT, () => {
+    console.log('Welcome to the TypeScript with Node.js and Express setup!');
     console.log(`Server is running on http://localhost:${PORT}`);
 });
