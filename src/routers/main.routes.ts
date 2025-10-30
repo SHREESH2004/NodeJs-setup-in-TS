36d0d7c58ac018625ef9bc44840bd773ea80585b
Shreesh Sanyal <shreeshsanyal@gmail.com>
2025-10-31 03:39:48 +0530

    Refactor username validation in /add route to ensure it is a string and not a number

--- src/routers/main.routes.ts
+++ src/routers/main.routes.ts
@@ -2,23 +2,30 @@ import { Router, Request, Response, NextFunction } from "express";
 import { getMainController } from "../controller/main.controller.js";
 import { body, validationResult } from "express-validator";
 
-const router = Router();
+const router: Router = Router();
 
 router.post(
   "/add",
   [
     body("username")
       .isString()
+      .isAlpha()
       .withMessage("Username must be a string")
       .notEmpty()
       .withMessage("Username is required")
+      .custom((value) => {
+        if (!isNaN(value)) {
+          throw new Error("Username cannot be a number");
+        }
+        return true;
+      })
   ],
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    next(); 
+    next();
   },
   getMainController
 );
